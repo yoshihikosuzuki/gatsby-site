b2fd8ea2780627a09ea8d639817f67c931f81b53
yoshihikosuzuki <ys.neoteny@gmail.com>
2020-02-02 04:44:58 +0900

    add google analytics

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: '',
+  googleAnalyticsId: 'UA-142767811-1',
   useKatex: true,
   menu: [
     {
